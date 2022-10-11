0e07d77e0ce5f56245ca499e19b20c6a2df24090
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-10-10 18:38:32 -0600

    :v:

--- utilidades.py
+++ utilidades.py
@@ -28,4 +28,4 @@ def dword(d): #Double word.
     
 def color(r, g, b): #Función que crea el color.
     #3 bytes. Retorna el color en bytes.
-    return bytes([int(b * 255), int(g * 255), int(r * 255)])
+    return bytes([int(b), int(g), int(r)])
